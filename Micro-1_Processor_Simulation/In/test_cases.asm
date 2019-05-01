d86e3eae6a57171fdf44755607ad1b0de9d6d5cc
Kevin Chevalier <kevin.chevalier10@gmail.com>
2019-05-01 10:30:09 -1000

    Change test cases + added comments

---
+++ Micro-1_Processor_Simulation/In/test_cases.asm
@@ -0,0 +1,220 @@
+loadc 1
+02
+loadc 0
+03fe
+store 0 1
+loadc 1
+03fe
+load 1 1
+loadc 0
+3ff
+store 0 1
+loadc 1
+03
+loadc 0
+03fd
+store 0 1
+loadc 1
+03fd
+load 1 1
+loadc 0
+3fe
+store 0 1
+loadc 1
+020
+loadc 0
+03fc
+store 0 1
+loadc 1
+03fc
+load 1 1
+loadc 0
+3fd
+store 0 1
+loadc 1
+03
+loadc 0
+03fb
+store 0 1
+loadc 1
+3ff
+load 1 1
+loadc 0
+03fa
+store 0 1
+loadc 0
+03fa
+load 0 0
+loadc 1
+03fb
+load 1 1
+mul 1 0
+loadc 0
+03fb
+store 0 1
+loadc 1
+02
+loadc 0
+03fa
+store 0 1
+loadc 1
+3fe
+load 1 1
+loadc 0
+03f9
+store 0 1
+loadc 0
+03f9
+load 0 0
+loadc 1
+03fa
+load 1 1
+mul 1 0
+loadc 0
+03fa
+store 0 1
+loadc 0
+03fa
+load 0 0
+loadc 1
+03fb
+load 1 1
+add 1 0
+loadc 0
+03fb
+store 0 1
+loadc 1
+3fd
+load 1 1
+loadc 0
+03fa
+store 0 1
+loadc 0
+03fa
+load 0 0
+loadc 1
+03fb
+load 1 1
+add 1 0
+loadc 0
+03fb
+store 0 1
+loadc 1
+03fb
+load 1 1
+loadc 0
+3fc
+store 0 1
+loadc 1
+02
+loadc 0
+03fa
+store 0 1
+loadc 1
+3fe
+load 1 1
+loadc 0
+03f9
+store 0 1
+loadc 0
+03f9
+load 0 0
+loadc 1
+03fa
+load 1 1
+lshift 1 0
+loadc 0
+03fa
+store 0 1
+loadc 1
+03fa
+load 1 1
+loadc 0
+3fb
+store 0 1
+loadc 1
+00
+loadc 0
+03f9
+store 0 1
+loadc 1
+03f9
+load 1 1
+loadc 0
+3fa
+store 0 1
+label1
+loadc 1
+3fd
+load 1 1
+loadc 0
+03f9
+store 0 1
+loadc 1
+03f9
+load 1 1
+loadc 0
+goto2
+if 1 0
+loadc 1
+3fa
+load 1 1
+loadc 0
+03f9
+store 0 1
+loadc 1
+3fd
+load 1 1
+loadc 0
+03f8
+store 0 1
+loadc 0
+03f8
+load 0 0
+loadc 1
+03f9
+load 1 1
+add 1 0
+loadc 0
+03f9
+store 0 1
+loadc 1
+03f9
+load 1 1
+loadc 0
+3fa
+store 0 1
+loadc 1
+3fd
+load 1 1
+loadc 0
+03f9
+store 0 1
+loadc 1
+01
+loadc 0
+03f8
+store 0 1
+loadc 0
+03f8
+load 0 0
+loadc 1
+03f9
+load 1 1
+rshift 1 0
+loadc 0
+03f9
+store 0 1
+loadc 1
+03f9
+load 1 1
+loadc 0
+3fd
+store 0 1
+loadc 1
+00
+loadc 0
+goto1
+if 1 0
+label2
+halt
